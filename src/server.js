7e9b58be09a46c2bb208d5c67022766e86f66e72
Alexander Shelyugov <ashelyugov@noreply.email>
2024-03-13 16:21:34 +0100

    Send a data sample in a separate endpoint

--- src/server.js
+++ src/server.js
@@ -1,54 +1,66 @@
-const express = require('express')
-const cors = require('cors')
-const { getCoordinates } = require('./emissionCoordinates');
-const { getMappingsBundleData, getMappingsWithUnknownCodeIds, retrieveKnownMappings } = require('./mappingsWithUnknownCode');
+const express = require("express");
+const cors = require("cors");
+const { getCoordinates } = require("./emissionCoordinates");
+const {
+  getMappingsBundleData,
+  getMappingsWithUnknownCodeIds,
+  retrieveKnownMappings,
+} = require("./mappingsWithUnknownCode");
+const path = require("path");
 
-const app = express()
-    .use(cors({
-        "origin": "*"
-    })
-)
+const app = express().use(
+  cors({
+    origin: "*",
+  })
+);
 
-app.get('/*', function (req, res, next) {
-    res.setHeader('Last-Modified', (new Date()).toUTCString());
-    next();
-})
+app.get("/*", function (req, res, next) {
+  res.setHeader("Last-Modified", new Date().toUTCString());
+  next();
+});
 
-app.get('/', (req, res) => {
-    res.send('Successful response.');
-})
+app.get("/", (req, res) => {
+  res.send("Successful response.");
+});
 
-app.get('/health', (req, res) => {
-    res.send('{ "status": "OK" }')
-})
+app.get("/health", (req, res) => {
+  res.send('{ "status": "OK" }');
+});
 
-app.get('/emissionsByCoordinates', (req, res) => {
-    const coordinates = getCoordinates()
-    res.send(coordinates)
-})
+app.get("/emissionsByCoordinates", (req, res) => {
+  const coordinates = getCoordinates();
+  res.send(coordinates);
+});
 
-app.get('/unknownMappings', (req, res) => {
-    const mappingsData = getMappingsBundleData()
-    res.send(mappingsData)
-})
+app.get("/unknownMappings", (req, res) => {
+  const mappingsData = getMappingsBundleData();
+  res.send(mappingsData);
+});
 
-app.get('/unknownMappingIds', (req, res) => {
-    const mappingIds = getMappingsWithUnknownCodeIds()
-    res.send(mappingIds)
-})
+app.get("/unknownMappingIds", (req, res) => {
+  const mappingIds = getMappingsWithUnknownCodeIds();
+  res.send(mappingIds);
+});
 
-app.get('/knownMappings', (req, res) => {
-    const knownMappings = retrieveKnownMappings()
-    res.send(knownMappings)
-})
+app.get("/knownMappings", (req, res) => {
+  const knownMappings = retrieveKnownMappings();
+  res.send(knownMappings);
+});
 
-app.post('/mappings', express.json(), (req, res) => {
-    if (!req.body) return response.sendStatus(400)
+app.post("/mappings", express.json(), (req, res) => {
+  if (!req.body) return response.sendStatus(400);
 
-    console.log("Saved all mappings")
-    console.log(req.body)
-    res.sendStatus(200)
-})
+  console.log("Saved all mappings");
+  console.log(req.body);
+  res.sendStatus(200);
+});
 
-const port = process.env.PORT
-app.listen(port, () => console.log(`Example app is listening on port ${port}.`))
+app.get('/data/ranges', function(req, res){
+    const file = `${__dirname}/samples/responseSample.json`;
+    res.sendFile(file); // Set disposition and send it.
+  });
+
+const port = process.env.PORT;
+app.listen(port, () =>
+  console.log(`Example app is listening on port ${port}.`)
+);
